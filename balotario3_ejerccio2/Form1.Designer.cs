345974b0ab8a90774832f5f2e5e0359c7b231498
JUAN CARLOS COTRINA ROMAN <43387467+jcfoxer@users.noreply.github.com>
2022-10-05 22:58:33 -0500

    Add files via upload

---
+++ balotario3_ejerccio2/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace balotario3_ejerccio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtunidades = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtcon = new System.Windows.Forms.TextBox();
+            this.txtdes = new System.Windows.Forms.TextBox();
+            this.txtpaga = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "unidades";
+            // 
+            // txtunidades
+            // 
+            this.txtunidades.Location = new System.Drawing.Point(215, 149);
+            this.txtunidades.Name = "txtunidades";
+            this.txtunidades.Size = new System.Drawing.Size(100, 20);
+            this.txtunidades.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(415, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "compra";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(415, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "descuento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(415, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "total a pagar";
+            // 
+            // txtcon
+            // 
+            this.txtcon.Location = new System.Drawing.Point(541, 40);
+            this.txtcon.Name = "txtcon";
+            this.txtcon.Size = new System.Drawing.Size(100, 20);
+            this.txtcon.TabIndex = 5;
+            // 
+            // txtdes
+            // 
+            this.txtdes.Location = new System.Drawing.Point(541, 104);
+            this.txtdes.Name = "txtdes";
+            this.txtdes.Size = new System.Drawing.Size(100, 20);
+            this.txtdes.TabIndex = 6;
+            // 
+            // txtpaga
+            // 
+            this.txtpaga.Location = new System.Drawing.Point(545, 166);
+            this.txtpaga.Name = "txtpaga";
+            this.txtpaga.Size = new System.Drawing.Size(100, 20);
+            this.txtpaga.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "nuevo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 311);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpaga);
+            this.Controls.Add(this.txtdes);
+            this.Controls.Add(this.txtcon);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtunidades);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtunidades;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtcon;
+        private System.Windows.Forms.TextBox txtdes;
+        private System.Windows.Forms.TextBox txtpaga;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ balotario3_ejerccio2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace balotario3_ejerccio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            // Declaración de Variables
+            
+            double unidades   ,icompra = 0, idescuento = 0, ipagar;
+
+            // Entrada de Datos
+            unidades = double.Parse(txtunidades.Text);
+
+            // Cálcular el Importe de la Compra
+            if (unidades >= 1 && unidades <= 25)
+                icompra = 27.7 * unidades;
+            if (unidades >= 26 && unidades <= 50)
+                icompra = 25.5 * unidades;
+            if (unidades >= 51 && unidades <= 75)
+                icompra = 23.5 * unidades;
+            if (unidades >= 76)
+                icompra = 21.5 * unidades;
+
+            // Cálcular el Importe de Descuento
+            if (unidades > 50)
+                idescuento = 0.15 * icompra;
+            if (unidades <= 50)
+                idescuento = 0.05 * icompra;
+
+            // Cálcular el importe a pagar
+            ipagar = icompra - idescuento;
+
+
+            // Salida de Resultados
+
+            txtcon.Text = icompra.ToString();
+            txtdes.Text = idescuento.ToString();
+            txtpaga.Text = ipagar.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            //me falat los mocmandos para salir y borrarr
+
+
+
+
+        }
+    }
+}
