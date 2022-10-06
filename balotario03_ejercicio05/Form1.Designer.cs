38f73c80852bca17a2b35b91bb6963a5ca42a4ac
JUAN CARLOS COTRINA ROMAN <43387467+jcfoxer@users.noreply.github.com>
2022-10-05 22:57:42 -0500

    uso de condiconal

---
+++ balotario03_ejercicio05/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace balotario03_ejercicio05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtimporte = new System.Windows.Forms.TextBox();
+            this.txthijos = new System.Windows.Forms.TextBox();
+            this.txtsu = new System.Windows.Forms.TextBox();
+            this.txtco = new System.Windows.Forms.TextBox();
+            this.txtbo = new System.Windows.Forms.TextBox();
+            this.txtsubru = new System.Windows.Forms.TextBox();
+            this.txtdes = new System.Windows.Forms.TextBox();
+            this.txtsune = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(309, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(309, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(309, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(309, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(309, 245);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(309, 284);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // txtimporte
+            // 
+            this.txtimporte.Location = new System.Drawing.Point(80, 165);
+            this.txtimporte.Name = "txtimporte";
+            this.txtimporte.Size = new System.Drawing.Size(100, 20);
+            this.txtimporte.TabIndex = 8;
+            // 
+            // txthijos
+            // 
+            this.txthijos.Location = new System.Drawing.Point(80, 229);
+            this.txthijos.Name = "txthijos";
+            this.txthijos.Size = new System.Drawing.Size(100, 20);
+            this.txthijos.TabIndex = 9;
+            // 
+            // txtsu
+            // 
+            this.txtsu.Location = new System.Drawing.Point(376, 84);
+            this.txtsu.Name = "txtsu";
+            this.txtsu.Size = new System.Drawing.Size(100, 20);
+            this.txtsu.TabIndex = 10;
+            // 
+            // txtco
+            // 
+            this.txtco.Location = new System.Drawing.Point(376, 125);
+            this.txtco.Name = "txtco";
+            this.txtco.Size = new System.Drawing.Size(100, 20);
+            this.txtco.TabIndex = 11;
+            // 
+            // txtbo
+            // 
+            this.txtbo.Location = new System.Drawing.Point(376, 169);
+            this.txtbo.Name = "txtbo";
+            this.txtbo.Size = new System.Drawing.Size(100, 20);
+            this.txtbo.TabIndex = 12;
+            // 
+            // txtsubru
+            // 
+            this.txtsubru.Location = new System.Drawing.Point(376, 203);
+            this.txtsubru.Name = "txtsubru";
+            this.txtsubru.Size = new System.Drawing.Size(100, 20);
+            this.txtsubru.TabIndex = 13;
+            // 
+            // txtdes
+            // 
+            this.txtdes.Location = new System.Drawing.Point(376, 242);
+            this.txtdes.Name = "txtdes";
+            this.txtdes.Size = new System.Drawing.Size(100, 20);
+            this.txtdes.TabIndex = 14;
+            // 
+            // txtsune
+            // 
+            this.txtsune.Location = new System.Drawing.Point(376, 284);
+            this.txtsune.Name = "txtsune";
+            this.txtsune.Size = new System.Drawing.Size(100, 20);
+            this.txtsune.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(577, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(577, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 331);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsune);
+            this.Controls.Add(this.txtdes);
+            this.Controls.Add(this.txtsubru);
+            this.Controls.Add(this.txtbo);
+            this.Controls.Add(this.txtco);
+            this.Controls.Add(this.txtsu);
+            this.Controls.Add(this.txthijos);
+            this.Controls.Add(this.txtimporte);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtimporte;
+        private System.Windows.Forms.TextBox txthijos;
+        private System.Windows.Forms.TextBox txtsu;
+        private System.Windows.Forms.TextBox txtco;
+        private System.Windows.Forms.TextBox txtbo;
+        private System.Windows.Forms.TextBox txtsubru;
+        private System.Windows.Forms.TextBox txtdes;
+        private System.Windows.Forms.TextBox txtsune;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ balotario03_ejercicio05/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace balotario03_ejercicio05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Declaración de variables
+            double impven, suelbas, comi, bonif, suelbru, desc, suelnet,hijos;
+            
+
+            // Entrada de datos
+            impven = double.Parse(txtimporte.Text);
+            hijos = double.Parse(txthijos.Text);
+
+            // Asignación del sueldo básico
+            suelbas = 600;
+
+            // Calcula la comisión
+            if (impven > 15000)
+                comi = 0.07 * impven;
+            else
+                comi = 0.05 * impven;
+
+            // Calcula la bonificación
+            if (hijos < 5)
+                bonif = 25 * hijos;
+            else
+                bonif = 22 * hijos;
+
+            // Calcula el sueldo bruto
+            suelbru = suelbas + comi + bonif;
+
+            // Calcula el descuento
+            if (suelbru > 3500)
+                desc = 0.15 * suelbru;
+            else
+                desc = 0.11 * suelbru;
+
+            // Calculo el sueldo neto
+            suelnet = suelbru - desc;
+
+
+
+            //salida de datos
+
+            txtsu.Text = suelbas.ToString();
+            txtco.Text = comi.ToString();
+            txtbo.Text = bonif.ToString();
+            txtsubru.Text = suelbru.ToString();
+            txtdes.Text = desc.ToString();
+            txtsune.Text = suelnet.ToString();
+            
+
+
+        }
+    }
+}
