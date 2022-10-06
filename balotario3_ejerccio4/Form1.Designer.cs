6e742a448e39e47ea38a957fc357c74c288c78e9
JUAN CARLOS COTRINA ROMAN <43387467+jcfoxer@users.noreply.github.com>
2022-10-05 22:59:20 -0500

    Add files via upload

---
+++ balotario3_ejerccio4/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace balotario3_ejerccio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cboturno = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtco = new System.Windows.Forms.TextBox();
+            this.txtdes = new System.Windows.Forms.TextBox();
+            this.txtpa = new System.Windows.Forms.TextBox();
+            this.txtca = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "turno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "cantidad";
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(205, 75);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(110, 20);
+            this.txtcantidad.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "procesar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(372, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "borrar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // cboturno
+            // 
+            this.cboturno.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.cboturno.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.cboturno.FormattingEnabled = true;
+            this.cboturno.Items.AddRange(new object[] {
+            "mañana",
+            "noche"});
+            this.cboturno.Location = new System.Drawing.Point(205, 12);
+            this.cboturno.Name = "cboturno";
+            this.cboturno.Size = new System.Drawing.Size(110, 21);
+            this.cboturno.TabIndex = 5;
+            this.cboturno.Text = "selecione un turno";
+            this.cboturno.SelectedIndexChanged += new System.EventHandler(this.button1_Click);
+            this.cboturno.SelectedValueChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "importe compra";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "importe  descuento";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(108, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "importe a  pagar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(94, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "caramelos de regalo";
+            // 
+            // txtco
+            // 
+            this.txtco.Location = new System.Drawing.Point(205, 141);
+            this.txtco.Name = "txtco";
+            this.txtco.Size = new System.Drawing.Size(100, 20);
+            this.txtco.TabIndex = 10;
+            // 
+            // txtdes
+            // 
+            this.txtdes.Location = new System.Drawing.Point(205, 183);
+            this.txtdes.Name = "txtdes";
+            this.txtdes.Size = new System.Drawing.Size(100, 20);
+            this.txtdes.TabIndex = 11;
+            // 
+            // txtpa
+            // 
+            this.txtpa.Location = new System.Drawing.Point(205, 220);
+            this.txtpa.Name = "txtpa";
+            this.txtpa.Size = new System.Drawing.Size(100, 20);
+            this.txtpa.TabIndex = 12;
+            // 
+            // txtca
+            // 
+            this.txtca.Location = new System.Drawing.Point(205, 261);
+            this.txtca.Name = "txtca";
+            this.txtca.Size = new System.Drawing.Size(100, 20);
+            this.txtca.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 323);
+            this.Controls.Add(this.txtca);
+            this.Controls.Add(this.txtpa);
+            this.Controls.Add(this.txtdes);
+            this.Controls.Add(this.txtco);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboturno);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cboturno;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtco;
+        private System.Windows.Forms.TextBox txtdes;
+        private System.Windows.Forms.TextBox txtpa;
+        private System.Windows.Forms.TextBox txtca;
+    }
+}
+
---
+++ balotario3_ejerccio4/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace balotario3_ejerccio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Declaración de variables
+            
+            double impcom, impdes, imppag,turno=0, cantidad=0,caramelos  ;
+
+
+            // Entrada de datos
+
+            cantidad = double.Parse(txtcantidad.Text);
+            string selectedItem = cboturno.Items[cboturno.SelectedIndex].ToString();
+
+
+           
+            // Calcula el importe de la compra
+            if (turno == 0)
+                impcom = 37.5 * cantidad;
+            else
+                impcom = 45.0 * cantidad;
+            // Calcula el importe del descuento
+            if (cantidad >= 15)
+                impdes = 0.08 * impcom;
+            else
+                impdes = 0.05 * impcom;
+            // Calcula el importe a pagar
+            imppag = impcom - impdes;
+            // Calcula los caramelos de obsequio
+            if (imppag > 200)
+                caramelos = 2 * cantidad;
+            else
+                caramelos = 0;
+
+            // Salida de resultados
+
+            txtco.Text = impcom.ToString();
+            txtdes.Text = impdes.ToString();
+            txtpa.Text = imppag.ToString();
+            txtca.Text = caramelos.ToString();
+
+
+
+        }
+    }
+}
