d01d812a4da55a5be9afcb5f75ad6e00afc00eba
JUAN CARLOS COTRINA ROMAN <43387467+jcfoxer@users.noreply.github.com>
2023-12-24 21:47:47 -0500

    Add files via upload

---
+++ buscar-elemntos -en-listbox/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace buscar_elemntos__en_listbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ltb1 = new System.Windows.Forms.ListBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(83, 64);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(120, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(548, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 90);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ltb1
+            // 
+            this.ltb1.FormattingEnabled = true;
+            this.ltb1.Location = new System.Drawing.Point(340, 64);
+            this.ltb1.Name = "ltb1";
+            this.ltb1.Size = new System.Drawing.Size(120, 95);
+            this.ltb1.TabIndex = 3;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(768, 100);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 261);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.ltb1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox ltb1;
+        private System.Windows.Forms.TextBox txt2;
+    }
+}
+
---
+++ buscar-elemntos -en-listbox/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buscar_elemntos__en_listbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            ltb1.Items.Add(txt1.Text);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int longitud;
+            int índice;
+            bool encontrado;
+            string elementoDeseado;
+            longitud = ltb1.Items.Count;
+            elementoDeseado = txt2.Text;
+            encontrado = false;
+            índice = 0;
+            while ((encontrado == false) && (índice < longitud))
+            {
+                if (Convert.ToString(ltb1.Items[índice]) == elementoDeseado)
+                {
+                    encontrado = true;
+                    MessageBox.Show("Elemento encontrado");
+                }
+                else
+                {
+                    índice++;
+
+
+
+                }
+            }
+
+        }
+    }
+}
