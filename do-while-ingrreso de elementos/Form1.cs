363ca4c274b43295eb7d4476852ab051b42e933f
JUAN CARLOS COTRINA ROMAN <43387467+jcfoxer@users.noreply.github.com>
2023-12-24 21:51:06 -0500

    Add files via upload

---
+++ do-while-ingrreso de elementos/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace do_while_ingrreso_de_elementos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Declaramos una variable para almacenar el número de alumnos
+            int numeroAlumnos = 0;
+
+            // Declaramos una variable para almacenar los datos de cada alumno
+            datos_alumnos alumno;
+
+            // Inicializamos el bucle
+            do
+            {
+                // Solicitamos los datos del alumno
+                alumno = new datos_alumnos();
+                alumno.IngresarDatos(this);
+
+                // Incrementamos el número de alumnos
+                numeroAlumnos++;
+
+                // Preguntamos si se desea ingresar otro alumno
+                MessageBox.Show("¿Desea ingresar otro alumno? (S/N)", "Matrícula de alumnos", MessageBoxButtons.YesNo);
+
+            } while (DialogResult.Yes == MessageBox.Show("¿Desea ingresar otro alumno? (S/N)", "Matrícula de alumnos", MessageBoxButtons.YesNo));
+
+            // Imprimimos el número de alumnos matriculados
+            textBox3.Text = "Se han matriculado " + numeroAlumnos + " alumnos";
+
+             class Alumno
+        {
+            public string nombre { get; set; }
+            public int edad { get; set; }
+            public string correo { get; set; }
+
+            public void IngresarDatos()
+            {
+                // Solicitamos el nombre del alumno
+                nombre = TextBox1.text();
+               
+
+                // Solicitamos la edad del alumno
+                form.TextBox2.Text = "";
+                form.TextBox2.Focus();
+                form.WaitForInputIdle();
+
+                // Solicitamos el correo electrónico del alumno
+                form.TextBox3.Text = "";
+                form.TextBox3.Focus();
+                form.WaitForInputIdle();
+
+                // Guardamos los datos del alumno
+                nombre = form.TextBox1.Text;
+                edad = int.Parse(form.TextBox2.Text);
+                correo = form.TextBox3.Text;
+
+
+
+
+
+            }
+        }
+    }
+}
---
+++ do-while-ingrreso de elementos/datos-alumnos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace do_while_ingrreso_de_elementos
+{
+    class datos_alumnos
+    {
+
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public string correo { get; set; }
+
+
+        public void ingresardatos(Form form)
+        {
+
+           
+
+
+        }
+
+
+
+    }
+}
